1df94349a80d3118e63b0296d976e47ae9e5e3f0
sahiln19 <146696728+sahiln19@users.noreply.github.com>
2024-04-22 13:48:39 +0530

    edit appointment pages

--- app3/public/site/assets/js/main.js
+++ app3/public/site/assets/js/main.js
@@ -33,12 +33,12 @@ Author: GrayGrids
 
 
         // show or hide the back-top-top button
-        var backToTo = document.querySelector(".scroll-top");
-        if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-            backToTo.style.display = "flex";
-        } else {
-            backToTo.style.display = "none";
-        }
+       // var backToTo = document.querySelector(".scroll-top");
+       // if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+           // backToTo.style.display = "flex";
+      //  } else {
+      //      backToTo.style.display = "none";
+       // }
     };
 
 
