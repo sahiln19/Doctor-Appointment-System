1233918df855c520ac34918c5f68e4c3c5026c62
sahiln19 <146696728+sahiln19@users.noreply.github.com>
2024-04-30 15:54:20 +0530

    ./

--- app3/src/Logout.js
+++ app3/src/Logout.js
@@ -8,28 +8,7 @@ import { withCookies } from 'react-cookie';
     componentDidMount() {
         const { cookies } = this.props;
         cookies.remove('userid', { path: '/' });
-        cookies.remove('email', { path: '/' });
-        cookies.remove('mydoctorid', { path: '/' });
-        cookies.remove('city', { path: '/' });
-        cookies.remove('serviceid', { path: '/' });
-        cookies.remove('service', { path: '/' });
-        cookies.remove('doctorid', { path: '/' });
-        cookies.remove('doctorname', { path: '/' });
-        cookies.remove('doctorqualification', { path: '/' });
-        cookies.remove('doctorimage', { path: '/' });
-        cookies.remove('doctoraddress', { path: '/' });
-        cookies.remove('doctormobile', { path: '/' });
-        cookies.remove('doctorcity', { path: '/' });
-        cookies.remove('doctorcityid', { path: '/' });
-        cookies.remove('doctorstate', { path: '/' });
-        cookies.remove('doctorstate id', { path: '/' });            
-        cookies.remove('doctorfees', { path: '/' });
-        cookies.remove('appointmentdate', { path: '/' });
-        cookies.remove('appointmenttime', { path: '/' });
-        cookies.remove('appointmentid', { path: '/' });
-        cookies.remove('appointmentstatus', { path: '/' });
-        cookies.remove('appointmentdoctorid', { path: '/' });
-        
+        cookies.remove('email', { path: '/' });    
         window.location = "/";
     }
     render() {
